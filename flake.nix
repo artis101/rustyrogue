5e5a9d0077362ee0797295855e2caa475cc47b16
Artis Avotins <artis.avotins@gmail.com>
2024-09-07 13:04:27 +0300

    initial

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A Rust roguelike game using SDL2 and TCOD";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustVersion
+            rust-analyzer
+            rustfmt
+            clippy
+            pkg-config
+            SDL2
+            SDL2_image
+            SDL2_ttf
+            SDL2_mixer
+            libcfg
+            libtcod
+          ];
+          shellHook = ''
+            echo "Rust roguelike development environment"
+            echo "-------------------------------------"
+            rustc --version
+            cargo --version
+            echo "-------------------------------------"
+          '';
+          RUST_SRC_PATH = "${rustVersion}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
